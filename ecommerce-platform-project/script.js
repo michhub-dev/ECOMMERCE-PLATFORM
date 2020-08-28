8bdf59ceb2676cc240c245b93121824051ff51cc
devolusegun <intelscope209@yahoo.com>
2020-08-28 21:43:52 +0100

    Login page fix

--- ecommerce-platform-project/script.js
+++ ecommerce-platform-project/script.js
@@ -1,6 +1,24 @@
-$("document").ready(function(){
-    $("button").click(function(){
-        $(".fa-cart-plus").text("added");
-    });
+var attempt = 3; // Variable to count number of attempts.
+// Below function Executes on click of login button.
+function validate(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    
+    if ( username == "michy@gmail.com" && password == "femcode123"){
+        alert ("Login successful");
+        window.location.href = "/home.html"; // Redirecting to other page.
+        //return true;
+    }
+    else{
+        attempt --;// Decrementing by one.
+        alert("You have left " + attempt + " attempt;");
+        // Disabling fields after 3 attempts.
 
-});
+        if( attempt == 0){
+            document.getElementById("username").disabled = true;
+            document.getElementById("password").disabled = true;
+            document.getElementById("submit").disabled = true;
+            return false;
+        }
+    }
+}
